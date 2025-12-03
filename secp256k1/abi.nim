f6655e4b5902c96b86a9d06d02ada5e3d372b9cd
bhartnett <benjamin@status.im>
2025-12-03 20:51:16 +0800

    Remove usage of quoteShell which doesn't compile on bare metal.

--- secp256k1/abi.nim
+++ secp256k1/abi.nim
@@ -1,5 +1,5 @@
 import strutils
-from os import DirSep, AltSep, quoteShell
+from os import DirSep, AltSep #, quoteShell
 
 const
   vendorPath = currentSourcePath.rsplit({DirSep, AltSep}, 1)[0] &
@@ -14,8 +14,8 @@ else:
 
 const compileFlags =
   "-DENABLE_MODULE_ECDH=1 -DENABLE_MODULE_RECOVERY=1 -DENABLE_MODULE_SCHNORRSIG=1 -DENABLE_MODULE_EXTRAKEYS=1" &
-  " -I" & quoteShell(internalPath) &
-  " -I" & quoteShell(srcPath) &
+  " -I\"" & internalPath & "\"" &
+  " -I\"" & srcPath & "\"" &
   asmFlags
 
 {.compile(srcPath & "/secp256k1.c", compileFlags).}
